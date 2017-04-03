ed5de08b1c82531d938905567a8ee727047b5fbb
= <karfannacif@gmail.com>
2017-04-02 23:35:11 -0300

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,27 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AppRoutingModule } from './app-routing.module';
+
+import { HeroService } from './heroes/hero.service';
+
 import { AppComponent } from './app.component';
-import { HeroComponent } from './hero/hero.component';
-import { HeroDetailComponent } from './hero/hero-detail.component';
-import { HeroService } from './hero/hero.service';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './heroes/hero-detail.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroComponent,
-    HeroDetailComponent
+    HeroesComponent,
+    HeroDetailComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
