e8d5e36cf8bd8685b73f15263193f95ff0e0b4cb
= <karfannacif@gmail.com>
2017-04-14 11:07:47 -0300

    Forms

---
+++ src/app/heroes/hero-form.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from "./hero";
+@Component({
+  selector: 'app-hero-form',
+  templateUrl: './hero-form.component.html',
+  styleUrls: ['./hero-form.component.css']
+})
+export class HeroFormComponent implements OnInit {
+
+  powers:string[] = [];
+  model: Hero;  
+  submitted: boolean = false;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.powers = ['Really Smart', 'Super Flexible', 'Super Hot', 'Weather Changer'];
+    this.model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+  }
+
+  onSubmit(){
+    this.submitted = true;
+  }
+
+  newHero(){
+    this.model = new Hero(42, '', '');
+  }
+  // TODO: Remove this when we're done
+  get diagnostic(){
+    return JSON.stringify(this.model);
+  }
+
+}
